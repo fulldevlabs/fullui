972a48e33a394336728aabafd5d482bf783783d3
lenardsilvester <lenardsilvester@icloud.com>
2024-06-24 09:43:17 +0200

    [stories] refactor: move available components to schema

---
+++ stories/src/schemas/components.ts
@@ -0,0 +1,25 @@
+import {
+  Badge,
+  Button,
+  Heading,
+  Icon,
+  Image,
+  Input,
+  Label,
+  Link,
+  Paragraph,
+  Textarea,
+} from 'fulldev-ui'
+
+export const FulldevUIComponents: any = {
+  Badge,
+  Button,
+  Heading,
+  Icon,
+  Image,
+  Input,
+  Label,
+  Link,
+  Paragraph,
+  Textarea,
+} as const
